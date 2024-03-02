304bb1fd5935a6e18d67983c35911a98a7e2e4b1
JayeshSharma <sjay26118@gmail.com>
2024-03-02 14:08:37 +0530

    Return types of Datatypes

--- learn.js
+++ learn.js
@@ -6,3 +6,18 @@ console.log(a+b)
  
 // let is used to declare any variable. Let has a block scope behaviour, 
 //so it only works inside the block scope.
+
+// Return Type of Variables in JavaScript
+/*Primitive Types */
+    Number  => Number
+    String => string
+    Boolean => string
+    null => object
+    undefined => undefined
+    Symbol => symbol
+    BigInt => bigint
+
+/* Non Primitives Datatypes*/
+    Array => object
+    Funtion => funtion 
+    Object => object
