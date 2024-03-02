28b4a0e8f20208d874249dcc06236c93e32e1462
JayeshSharma <sjay26118@gmail.com>
2024-03-02 12:52:39 +0530

    JS-Basic

--- learn.js
+++ learn.js
@@ -4,4 +4,5 @@ let a = 34
 let b = 33 
 console.log(a+b)
  
-// let is used to declare any variable. Let has a block scope behaviour, so it only works inside the block scope.
+// let is used to declare any variable. Let has a block scope behaviour, 
+//so it only works inside the block scope.
